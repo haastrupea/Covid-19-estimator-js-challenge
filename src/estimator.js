4b251f1027ba0a6cee295980a3c8579d9cf6f013
Haastrup Elijah <haastrupea@gmail.com>
2020-04-12 16:36:23 +0100

    es lint fix
    reformat code to comply with eslint standard

--- src/estimator.js
+++ src/estimator.js
@@ -1,67 +1,67 @@
-const normaliseDuration = (timeToElapse,periodType) => {
-    let period = Number(timeToElapse);
+// wrapper function for Math.trunc
+const trunc = (number) => Math.trunc(number);
 
-    switch (periodType.toLowerCase) {
-        case "days":
-            period=(period/3);
-            break;
+const normaliseDuration = (timeToElapse, periodType) => {
+  let period = Number(timeToElapse);
 
-        case "weeks":
-            period=(period/3) * 7;
-            break;
-        case "months":
-            period=(period/3) * 30;
-            break;
-        case "years":
-            period=(period/3) * 366;
-            break;
-    }
-    return Math.trunc(period);
-}
+  switch (periodType.toLowerCase) {
+    case 'days':
+      period /= 3;
+      break;
+    case 'weeks':
+      period = (period / 3) * 7;
+      break;
+    case 'months':
+      period = (period / 3) * 30;
+      break;
+    case 'years':
+      period = (period / 3) * 366;
+      break;
+    default:
+  }
+  return trunc(period);
+};
+
+// estimator
+const estimator = (data, mutiplier) => {
+  // constant values
+  const needICU = 0.05;// estimated % of severe +ve cases that will require ICU care
+  const needToRecover = 0.15; // estimated % of severe +ve cases that need hospital to recover
+  const bed = 0.35; // % bed available in hospitals for severe COVID-19 positive patients.
+  const needVentilator = 0.02; // estimated % of severe positive cases that will require ventilators
+  const earnByPeople = 0.65; // of the region (the majority) earn $1.5 a day
+  const USDEarn = 1.5; // 65% the region (the majority) earn $1.5 a day
+  // data needed
+  const {
+    periodType, timeToElapse, reportedCases, totalHospitalBeds
+  } = data;
+  const normalise = normaliseDuration(Number(timeToElapse), periodType);
+  const currentlyInfected = trunc(Number(reportedCases) * Number(mutiplier));
+  const infectionsByRequestedTime = trunc(currentlyInfected * normalise * 2);
+  const severeCasesByRequestedTime = trunc(infectionsByRequestedTime * needToRecover);
+  const hBBRT = trunc((Number(totalHospitalBeds) * bed) - severeCasesByRequestedTime);
+  const casesForICUByRequestedTime = trunc(infectionsByRequestedTime * needICU);
+  const casesForVentilatorsByRequestedTime = trunc(infectionsByRequestedTime * needVentilator);
+  const dollarsInFlight = trunc((infectionsByRequestedTime * earnByPeople * USDEarn) * normalise);
 
 
-//estimator
-const estimator = (data,mutiplier) => {
-    //constant values
-    const PercentNeedICU = .05;//This is the estimated percentage of severe positive cases that will require ICU care
-    const PercentNeedHospitalToRecover = .15 //estimated percentage of severe positive cases that will require hospitalization to recover
-    const PercentbedAvailable = .35 // percentage bed availability in hospitals for severe COVID-19 positive patients.
-    const PercentNeedVentilator = .02 // estimated percentage of severe positive cases that will require ventilators
-    const PercentUSDEarnByPeopleInRegion= .65 //of the region (the majority) earn $1.5 a day
-    const USDEarnPerDay = 1.5 //65% the region (the majority) earn $1.5 a day
-    //data needed
-    const timeToElapse = Number(data.timeToElapse);
-    const periodType = data.periodType;
-    const normaliseInfectionDuration = normaliseDuration(timeToElapse,periodType);
-
-    const currentlyInfected = Math.trunc(Number(data.reportedCases) * Number(mutiplier));
-    const infectionsByRequestedTime = Math.trunc(currentlyInfected * normaliseInfectionDuration * 2);
-    const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * PercentNeedHospitalToRecover);
-    const hospitalBedsByRequestedTime = Math.trunc((Number(data.totalHospitalBeds)* PercentbedAvailable ) - severeCasesByRequestedTime);
-    const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * PercentNeedICU);
-    const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * PercentNeedVentilator);
-    const dollarsInFlight = Math.trunc((infectionsByRequestedTime * PercentUSDEarnByPeopleInRegion * USDEarnPerDay) * normaliseInfectionDuration);
-
-
-
-    return {
-        currentlyInfected,
-        infectionsByRequestedTime,
-        severeCasesByRequestedTime,
-        hospitalBedsByRequestedTime,
-        casesForICUByRequestedTime,
-        casesForVentilatorsByRequestedTime,
-        dollarsInFlight
-    };
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime: hBBRT,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
 };
 
 
 const covid19ImpactEstimator = (data) => {
+  const impact = estimator(data, 10);
+  const severeImpact = estimator(data, 50);
 
-    const impact = estimator(data,10);
-    const severeImpact = estimator(data,50);
-
-    return {data,impact,severeImpact};
+  return { data, impact, severeImpact };
 };
 
 export default covid19ImpactEstimator;
